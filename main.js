ca737623134be072e0b72c35af0e71eb134515ef
belmer96 <elmerblake1500@gmail.com>
2023-01-12 13:26:47 -0600

    add round functionality which outputs winner up to 5 rounds

--- main.js
+++ main.js
@@ -6,29 +6,60 @@ function getComputerChoice(){
 
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
 // need to prompt human for their choice
 
 function promptForChoice() {
     while(true){
-        let userInput = prompt("Please choose between " + "rock" + ", " + "paper" + ", " + "scissors" + ":");
-        if (userInput === "rock" || userInput === "paper" || userInput === "scissors") {
-            return userInput;
+        let userInput = prompt("Please choose between " + "Rock" + ", " + "Paper" + ", " + "Scissors" + ":");
+        let modifyuserInput = userInput.toLowerCase();
+        if (modifyuserInput === "rock" || modifyuserInput === "paper" || modifyuserInput === "scissors") {
+            return modifyuserInput;
             break;
             // console.log("You chose " + userInput + ".");
-        } else {
+        }else {
             console.log("Invalid choice, please try again.");
-            promptForChoice();
+            // promptForChoice();
         }
     }
     
 }
 
-console.log(promptForChoice());
+function newchoicepc(){   
+    let computerchoice=getComputerChoice();
+    return computerchoice;
+    }
+function newchoiceuser(){   
+    let userchoice=promptForChoice();
+    return userchoice;
+}
+// console.log("computer choice "+newchoicepc()+" userchoice "+newchoiceuser());
 
 
-
+// console.log(promptForChoice());
 
 
 //need to compare the two and output a winner
+function playround(){
+    computerchoice=newchoicepc();
+    userchoice=newchoiceuser();
+    if (computerchoice === userchoice){
+        console.log("Tie");
+    } else if(computerchoice === "rock"&& userchoice ==="scissors" || computerchoice==="paper" && userchoice==="rock"
+    || computerchoice==="scissors"&& userchoice==="paper"){
+        console.log("Computer wins")
+    } else{
+        console.log("You Win!")
+    }
+    }
+
+function playrounds(){
+    for (let i = 0; i < 5; i++) {
+        playround();
+     }
+}
+
+
+
+console.log(playrounds())
